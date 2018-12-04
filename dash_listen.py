e2acdffaf69e63ef1d8b4bb3c65e6ecc9f7edf0f
Eddie Tillistrand <ejt4x@users.noreply.github.com>
2018-12-04 03:12:41 +0000

    Move macs to config file

--- dash_listen.py
+++ dash_listen.py
@@ -6,16 +6,11 @@ import config
 import requests
 import os
 import sys
-
-macs = {
-        'a4:77:33:92:b4:82': 'pup'
-        }
+from config import ifttt_key, macs
 
 ifttt_url = 'https://maker.ifttt.com/trigger/{event}/with/key/{key}'
-ifttt_key = config.ifttt_key
 
 #log.basicConfig(level=log.DEBUG)
-
 
 def button_pressed(event):
     request_url = ifttt_url.format(event=event, key=ifttt_key)
